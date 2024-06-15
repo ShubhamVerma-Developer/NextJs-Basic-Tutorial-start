ebbd6247cbddfde46d0dc65c6e3c639aaa4f3c90
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-15 18:16:17 +0530

    caching and validate

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 
-const page = () => {
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+  console.log(users);
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default page;
+export default UsersPage;
