28f385052711c94aed4814f32f0354c3b6472513
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-15 18:44:42 +0530

    no cache when build

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -23,13 +23,14 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
+    cache: "no-store",
   });
   const users: User[] = await res.json();
-  console.log(users);
+  // console.log(users);
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
